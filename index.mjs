0223862a7b03e45186f2399a8b2c8eadc81e0c01
Israajddn <Israajddn@gmail.com>
2023-11-18 12:25:46 -0600

    Creating Routes

--- index.mjs
+++ index.mjs
@@ -1,12 +1,16 @@
 import express from "express";
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+import customers from './routes/customers.mjs'; 
+
+//MIDDLEWARE
+app.use('/restaurants', restaurants);
 
 // Error Handling Middleware
 app.use((err, req, res, next)=>{
-    res.status(500).send("Something went wrong!")
-})
+    res.status(500).send("Something went wrong!");
+});
 
 app.listen(port, ()=>{
-    console.log(`Server is listening on port: ${port}`)
-})
+    console.log(`Server is listening on port: ${port}`);
+});
